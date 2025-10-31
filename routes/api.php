d30ad4cdaf2897875749392a99daf1dd8e193747
Md Jahidul Islam <jahidcse181@gmail.com>
2025-10-31 19:58:39 +0600

    routes modified

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
-    Route::apiResource('suppliers', SupplierController::class);
     Route::get('suppliers/trash', [SupplierController::class, 'trash']);
     Route::post('suppliers/{id}/restore', [SupplierController::class, 'restore']);
+    Route::apiResource('suppliers', SupplierController::class);
 
-    Route::apiResource('fabrics', FabricController::class);
     Route::get('fabrics/trash', [FabricController::class, 'trash']);
     Route::post('fabrics/{id}/restore', [FabricController::class, 'restore']);
     Route::get('fabrics/{id}/print-barcode', [FabricController::class, 'printBarcode']);
+    Route::apiResource('fabrics', FabricController::class);
 
     Route::apiResource('fabric-stocks', FabricStockController::class)->only(['index', 'store']);
 
